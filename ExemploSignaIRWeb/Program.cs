ca85371faef5936cfd14434237d49e7f560195d6
alexa <alexa@LAPTOP-UF0HUGSI>
2024-07-08 20:32:54 -0300

    Add project files.

---
+++ ExemploSignaIRWeb/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExemploSignaIRWeb.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
+
+app.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+
+app.Run();
